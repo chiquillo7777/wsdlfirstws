545fefeb118cd11c8abb68183076a0317e1b73f5
carlos chiquillo <carlosjosechiquillolopez@gmail.com>
2020-01-28 13:43:37 -0600

    adding webconfiguration class, now app is ready to test

---
+++ src/main/java/com/carloschiquillo/ws/soap/config/WebServiceConfig.java
@@ -0,0 +1,27 @@
+package com.carloschiquillo.ws.soap.config;
+
+import javax.xml.ws.Endpoint;
+
+import org.apache.cxf.Bus;
+import org.apache.cxf.jaxws.EndpointImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.carloschiquillo.ws.soap.CustomerOrdersWsImpl;
+
+
+@Configuration
+public class WebServiceConfig {
+	
+	@Autowired
+	private Bus bus;
+	
+	@Bean
+	public Endpoint endpoint() {
+		Endpoint endpoint = new EndpointImpl(bus, new CustomerOrdersWsImpl());
+		endpoint.publish("/customerordersservice");
+		
+		return endpoint;
+	}
+}
